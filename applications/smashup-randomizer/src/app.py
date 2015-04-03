864e171244c6c7a5e0e0f2c99f73e64afb3a1aa6
CJ Davis <Christopher.Davis@cqlcorp.com>
2015-04-03 17:14:01 -0400

    Beginning attempt of PostgreSQL connection

--- applications/smashup-randomizer/src/app.py
+++ applications/smashup-randomizer/src/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+import psycopg2, psycopg2.extras
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template('index.html')
+  #g.db = psycopg2.connect("dbname=smashup-randomizer user=smashup-user", connection_factory=psycopg2.extras.RealDictConnection)
+  foo = "bar"
+  return render_template('index.html')
 
 if __name__ == "__main__":
   app.run(debug=True)
