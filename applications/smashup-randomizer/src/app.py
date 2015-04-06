331d8069248a4b871d11d049c115198224608094
CJ Davis <cj@cjdavis.me>
2015-04-06 18:46:25 -0400

    Pass exceptions along to uWSGI and use a library to help display the error messages (NOT TO BE USED IN PRODUCTION)

--- applications/smashup-randomizer/src/app.py
+++ applications/smashup-randomizer/src/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+from werkzeug.debug import DebuggedApplication
+
 app = Flask(__name__)
+app.debug = True
+app.wsgi_app = DebuggedApplication( app.wsgi_app, True )
 
 @app.route("/")
 def index():
 	return render_template('index.html')
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run()
