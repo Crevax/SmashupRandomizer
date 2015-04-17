6d44dd509e4f71d451a12d0e466756d9e2ddbc31
CJ Davis <Christopher.Davis@cqlcorp.com>
2015-04-17 16:32:42 -0400

    Refactor configuration to use variables

--- applications/smashup-randomizer/src/app.py
+++ applications/smashup-randomizer/src/app.py
@@ -2,9 +2,17 @@ from flask import abort, Flask, render_template
 from werkzeug.debug import DebuggedApplication
 import psycopg2, psycopg2.extras
 
+# configuration - will be refactored and moved later
+DEBUG       = True
+SECRET_KEY  ='development key'
+DB_HOST     = 'localhost'
+DB_NAME     = 'smashup_randomizer'
+DB_USER     = 'smashup_user'
+DB_PASS     = '123four'
+
 app = Flask(__name__)
-app.debug = True
-app.wsgi_app = DebuggedApplication( app.wsgi_app, True )
+app.config.from_object(__name__)
+app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
 
 @app.route("/")
 def index():
@@ -12,7 +20,7 @@ def index():
 
 @app.route("/db_check")
 def db_check():
-  conn = psycopg2.connect("host=localhost dbname=smashup_randomizer user=smashup_user password=123four", connection_factory=psycopg2.extras.RealDictConnection)
+  conn = psycopg2.connect(host=app.config['DB_HOST'], database=app.config['DB_NAME'], user=app.config['DB_USER'], password=app.config['DB_PASS'], connection_factory=psycopg2.extras.RealDictConnection)
   conn.close()
   return "It Works!"
 
