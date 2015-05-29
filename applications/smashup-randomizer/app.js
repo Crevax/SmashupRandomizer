e4adb5664827f1abc070faf29b3e9e18b4925bfe
CJ Davis <Christopher.Davis@cqlcorp.com>
2015-05-29 15:00:54 -0400

    Create 'Hello World' in the client app with React

---
+++ applications/smashup-randomizer/app.js
@@ -0,0 +1,12 @@
+var App = React.createClass({
+  render: function() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+});
+
+React.render(
+  <App />,
+  document.getElementById('content')
+);
